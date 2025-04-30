972e7a78f8a8c25fa415fd6cac49842c95558318
GibsonCS <gibson.cruzds@gmail.com>
2025-04-29 22:52:49 -0300

    bind study

---
+++ bind-call-apply/bind.js
@@ -0,0 +1,21 @@
+'use-strict'
+import {watch} from 'node:fs'
+import {readFile} from 'fs/promises'
+
+class File {
+    monitor(event, filename) {
+        console.log(event, filename)
+
+        this.showContent(filename)
+    }
+
+    async showContent (filename) {
+        console.log((await readFile(filename)).toString())
+    }
+}
+const file = new File()
+// "this" context is lost
+const functionWithoutThisContext =  file.watch
+
+// With bind, "this" context of the file is mainteined
+watch(`${process.cwd()}/bind.js`, file.monitor.bind(file))
