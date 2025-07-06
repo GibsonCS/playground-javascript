d376831671be138a8a16875b58514f92a7d16756
GibsonCS <gibson.cruzds@gmail.com>
2025-07-06 11:56:06 -0300

    chore: exercicie01

--- array-exercicies/index.js
+++ array-exercicies/index.js
@@ -1,2 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const fruits = ["maracuja", "pera", "uva", "banana", "manga", "jabuticaba"];
+
+//exercicie01
+const mapToDouble = (numbers) => {
+  return numbers.map((n) => n * 2);
+};
+console.log(`exercicie01: ${mapToDouble(numbers)}`);
