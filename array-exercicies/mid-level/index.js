971c1c9add8af778ef7a4e8eab2dcd21331f65cc
GibsonCS <gibson.cruzds@gmail.com>
2025-07-06 19:25:15 -0300

    chore: exercice10

--- array-exercicies/mid-level/index.js
+++ array-exercicies/mid-level/index.js
@@ -29,3 +29,12 @@ const descendingOrder = (numbers) => {
   return numbers.sort((a, b) => b - a);
 };
 console.log(`exercice09: ${descendingOrder(numbers)}`);
+
+//exercice10
+const lastThreeElement = (array) => {
+  if (array.length < 3) return array;
+  return array.slice(array.length - 3);
+};
+const numbers2 = [1, 2, 4, 6, 8, 9, -4, 7, 60, 10, 9, 12, 61];
+const twoElements = [1, 2];
+console.log(lastThreeElement(numbers2));
