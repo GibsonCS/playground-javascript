0d42afd4b05bcbeb6a758d4af968ad3e1694a37b
GibsonCS <gibson.cruzds@gmail.com>
2025-07-06 18:53:54 -0300

    chore: exercice06

---
+++ array-exercicies/mid-level/index.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 4, 6, 8, 9, -4, 7, 60, 10, 9, 12, 61];
+const animals = ["Lion", "Monkey", "Zebra", "Tiger", "Camel"];
+
+//exercicie06
+const firstNumberGreaterThanTen = (numbers) => {
+  return numbers.find((n) => n > 10);
+};
+console.log(`exercice06: ${firstNumberGreaterThanTen(numbers)}`);
