074f5dfcf1b752a26e84cf6b4c65d410a3982a12
GibsonCS <gibson.cruzds@gmail.com>
2025-07-06 12:00:32 -0300

    chore: exercicie2

--- array-exercicies/index.js
+++ array-exercicies/index.js
@@ -6,3 +6,9 @@ const mapToDouble = (numbers) => {
   return numbers.map((n) => n * 2);
 };
 console.log(`exercicie01: ${mapToDouble(numbers)}`);
+
+//exercicie02
+const pairNumbers = (numbers) => {
+  return numbers.filter((n) => n % 2 === 0);
+};
+console.log(`exercicie02: ${pairNumbers(numbers)}`);
