2ed06e677bfe8b76c73c6e21af5fbe774944e180
GibsonCS <gibson.cruzds@gmail.com>
2025-07-06 19:29:46 -0300

    chore: exercice10

--- array-exercicies/mid-level/index.js
+++ array-exercicies/mid-level/index.js
@@ -21,12 +21,12 @@ console.log(`exercice08: ${allStringsStartWithUperCase(animals)}`);
 
 //exercice09
 const ascendingOrder = (numbers) => {
-  return numbers.sort((a, b) => a - b);
+  return [...numbers].sort((a, b) => a - b);
 };
 console.log(`exercice09: ${ascendingOrder(numbers)}`);
 
 const descendingOrder = (numbers) => {
-  return numbers.sort((a, b) => b - a);
+  return [...numbers].sort((a, b) => b - a);
 };
 console.log(`exercice09: ${descendingOrder(numbers)}`);
 
@@ -35,6 +35,5 @@ const lastThreeElement = (array) => {
   if (array.length < 3) return array;
   return array.slice(array.length - 3);
 };
-const numbers2 = [1, 2, 4, 6, 8, 9, -4, 7, 60, 10, 9, 12, 61];
 const twoElements = [1, 2];
-console.log(lastThreeElement(numbers2));
+console.log(lastThreeElement(numbers));
